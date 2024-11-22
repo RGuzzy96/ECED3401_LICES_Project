d966ea3bf6a2bb551c99ef090e877694feecb089
RGuzzy96 <ryanguzzwell@gmail.com>
2024-11-22 06:27:35 -0400

    more resizing handling

---
+++ ECED3401_LICES_Project/storage.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+- Header file with layers
+	- Each contains a pointer to the start of its block of the file for the layer map
+	- First avail
+	- Deleted list
+
+OR, have cells link to each other (don't need this if we store contiguously)
+*/
